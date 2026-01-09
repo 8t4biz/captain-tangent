41859a3ab86c2533f112ef3276b2f29fef4c9a6d
Julien Carcaly <jusmacbookpro@Jus-MacBook-Pro.local>
2026-01-09 11:58:00 -0500

    Fix: add base path for Vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // ✅ IMPORTANT pour Render (ou GitHub Pages)
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
